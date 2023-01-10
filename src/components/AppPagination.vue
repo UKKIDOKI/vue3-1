10afbfe70c70c32d666ed6da77676b4042fdecbe
jaeukkim <jaeukkim@lemonhc.com>
2023-01-10 18:05:07 +0900

    10일

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="mt-5" aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item" :class="{ disabled: props.page === 1 }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Previous"
+          @click.prevent="prevPage"
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li
+        v-for="page in props.pageCount"
+        :key="page"
+        class="page-item"
+        :class="{ active: props.page === page }"
+      >
+        <a class="page-link" href="#" @click.prevent="changePage(page)">{{
+          page
+        }}</a>
+      </li>
+      <li class="page-item" :class="{ disabled: props.page >= pageCount }">
+        <a
+          class="page-link"
+          href="#"
+          aria-label="Next"
+          @click.prevent="nextPage"
+        >
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+const props = defineProps({
+  page: null,
+  pageCount: null,
+});
+defineEmits(['update:', 'update:content']);
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/posts/PostCreateView.vue
+++ src/views/posts/PostCreateView.vue
@@ -2,21 +2,12 @@
   <div>
     <h2>게시글 등록</h2>
     <hr class="my-4" />
-    <form @submit.prevent="save(title, content)">
-      <div class="mb-3">
-        <label for="titie" class="form-label">제목</label>
-        <input type="text" class="form-control" id="titie" v-model="title" />
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea
-          class="form-control"
-          id="content"
-          rows="3"
-          v-model="content"
-        ></textarea>
-      </div>
-      <div class="pt-4">
+    <PostForm
+      v-model:title="form.title"
+      v-model:content="form.content"
+      @submit.prevent="save(form.title, form.content)"
+    >
+      <template #actions>
         <button
           type="button"
           class="btn btn-outline-dark me-2"
@@ -25,8 +16,8 @@
           목록
         </button>
         <button class="btn btn-primary">저장</button>
-      </div>
-    </form>
+      </template>
+    </PostForm>
   </div>
 </template>
 
@@ -34,8 +25,15 @@
 import { useRouter } from 'vue-router';
 import { createPost } from '@/api/posts';
 import dayjs from 'dayjs';
+import PostForm from '@/components/posts/PostForm.vue';
+import { ref } from 'vue';
 
 const router = useRouter();
+
+const form = ref({
+  title: null,
+  content: null,
+});
 
 const goPostList = () => {
   router.push({
@@ -43,13 +41,12 @@ const goPostList = () => {
   });
 };
 
-const save = async (title, content) => {
-  console.log(title, content);
+const save = async () => {
   try {
     const createdAt = dayjs().format('YYYY-MM-DD');
     await createPost({
-      title,
-      content,
+      title: form.value.title,
+      content: form.value.content,
       createdAt,
     });
     router.push({
